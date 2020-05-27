8a470ee997570756552b4e44285974ff33498835
Deepak Rout <deepakrout@gmail.com>
2020-05-27 03:51:46 -0400

    revertback

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,12 +3,11 @@ import { Config } from '@stencil/core';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  namespace: 'awesome-element',
-  bundles: [{components: ['my-first-component']}],
   globalScript: 'src/global/app.ts',
   globalStyle: 'src/global/app.css',
   taskQueue: 'async',
   outputTargets: [{
-    type: 'dist'
+    type: 'www',
+    serviceWorker: null
   }],
 };
